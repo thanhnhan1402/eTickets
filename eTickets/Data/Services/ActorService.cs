9b95caf92fded46d93eb640d28432e5c16801259
thanhnhan1402 <nhanttse151229@fpt.edu.vn>
2022-07-25 01:19:24 +0700

    Add Service classes

---
+++ eTickets/Data/Services/ActorService.cs
@@ -0,0 +1,38 @@
+﻿using eTickets.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTickets.Data.Services;
+
+public class ActorService : IActorService
+{
+    private readonly AppDbContext _context;
+    public ActorService(AppDbContext context)
+    {
+        _context = context;
+    }
+    public void Add(Actor actor)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<Actor>> GetAll()
+    {
+        var result = await _context.Actors.ToListAsync();
+        return result;
+    }
+
+    public Actor GetById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Actor Update(int id, Actor newActor)
+    {
+        throw new NotImplementedException();
+    }
+}
