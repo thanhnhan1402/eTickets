bb0d29387f4129307be2169441d89cee19417512
thanhnhan1402 <nhanttse151229@fpt.edu.vn>
2022-07-25 01:18:40 +0700

    Refactor Models classes

--- eTickets/Models/Cinema.cs
+++ eTickets/Models/Cinema.cs
@@ -6,7 +6,9 @@ public class Cinema
 {
     [Key]
     public int Id { get; set; }
+    [Display(Name = "Cinema's Logo")]
     public string Logo { get; set; }
+    [Display(Name = "Cinema's Name")]
     public string Name { get; set; }
     public string Description { get; set; }
     //Relationships
