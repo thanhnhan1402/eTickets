3a4ba268e182b078c0972bca3ee7786e11dc8a27
thanhnhan1402 <nhanttse151229@fpt.edu.vn>
2022-07-24 18:15:47 +0700

    Refactor code

--- eTickets/Program.cs
+++ eTickets/Program.cs
@@ -1,7 +1,13 @@
+using eTickets.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//DbContext configuration
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
@@ -24,4 +30,6 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+//Seed database
+AppDbInitializer.Seed(app);
 app.Run();
