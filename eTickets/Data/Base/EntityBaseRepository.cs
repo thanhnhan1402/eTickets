1d0e9ce39edd2d37e2c06c48a08f898d44a45fff
thanhnhan1402 <nhanttse151229@fpt.edu.vn>
2022-08-03 17:08:25 +0700

    Adding Entity Base Repository

---
+++ eTickets/Data/Base/EntityBaseRepository.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace eTickets.Data.Base
+{
+    public class EntityBaseRepository<T> : IEntityBaseRepository<T> where T : class, IEntityBase, new()
+    {
+        private readonly AppDbContext _context;
+        public EntityBaseRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(T entity) => await _context.Set<T>().AddAsync(entity);          
+        
+        public async Task DeleteAsync(int id)
+        {
+            var entity = await _context.Set<T>().FirstOrDefaultAsync(a => a.Id == id);
+            EntityEntry entityEntry = _context.Entry<T>(entity);
+            entityEntry.State = EntityState.Deleted;
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync() => await _context.Set<T>().ToListAsync();
+            
+        public async Task<T> GetByIdAsync(int id) => await _context.Set<T>().FirstOrDefaultAsync(a => a.Id == id);
+            
+
+        public async Task UpdateAsync(int id, T entity)
+        {
+            EntityEntry entityEntry = _context.Entry<T>(entity);
+            entityEntry.State = EntityState.Modified;
+        }
+    }
+}
